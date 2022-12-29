0dee7d754750d3e1cb7d04f29d46dcc878d25ea8
jeffr-k <jeff.cofos@gmail.com>
2022-12-29 10:04:18 +0900

    env

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use actix_web::{App, HttpServer, Responder, web};
+extern crate dotenv;
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use dotenv::dotenv;
+use std::env;
 
-async  fn health_check() -> impl Responder {
+
+async  fn health_check(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("hello, world!");
-    format!("Hello {}!", &name)
+    format!("{}!", &name)
 }
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
     HttpServer::new(|| {
         App::new()
-            .router("/", web::get().to(health_check))
-            .router()
+            .route("/", web::get().to(health_check))
     })
-    .bind(("127.0.0.1:8000"))?
+    .bind((
+        env::var("SERVER_HOST").unwrap(),
+        env::var("SERVER_PORT").unwrap().parse::<u16>().unwrap())
+    )?
     .run()
     .await
 }
